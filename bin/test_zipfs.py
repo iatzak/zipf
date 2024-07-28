1836d3919d8a4670201011b0ddd722ad4ed2b62d
Arthur Lacerda Essel <arthuressel@gmail.com>
2024-07-28 12:17:58 +0200

    Write test for alpha parameter

--- bin/test_zipfs.py
+++ bin/test_zipfs.py
@@ -1,5 +1,8 @@
 from collections import Counter
+import pytest
+import numpy as np
 import countwords
+import plotcounts
 
 
 def test_word_count():
@@ -16,3 +19,22 @@ def test_word_count():
     with open('test_data/risk.txt', 'r') as reader:
         actual_result = countwords.count_words(reader)
     assert actual_result == expected_result
+
+
+def test_alpha():
+    """Test the calculation of the alpha parameter.
+    The test word counts satisfy the relationship
+    r = cf**(-1/alpha), where
+    r is the rank,
+    f is the word count, and
+    c is a constant of proportionality.
+
+    To generate test word counts for an expected alpha
+    of 1.0, a maximum word frequency of 600 is used
+    (i.e. c = 600 and r ranges from 1 to 600)
+    """
+    max_freq = 600
+    counts = np.floor(max_freq / np.arange(1, max_freq + 1))
+    actual_alpha = plotcounts.get_power_law_params(counts)
+    expected_alpha = pytest.approx(1.0, abs=0.01)
+    assert actual_alpha == expected_alpha
